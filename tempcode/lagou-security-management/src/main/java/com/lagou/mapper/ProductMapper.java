1a7f8ce77c5c71cb9b4a8ac7cacd59a556e7a301
Iban-blade <2734319366@qq.com>
2021-09-29 23:00:19 +0800

    update

--- tempcode/lagou-security-management/src/main/java/com/lagou/mapper/ProductMapper.java
+++
@@ -1,7 +0,0 @@
-package com.lagou.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lagou.domain.Product;
-
-public interface ProductMapper extends BaseMapper<Product> {
-}
--- tempcode/lagou-security-management/src/main/java/com/lagou/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.lagou.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lagou.domain.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
